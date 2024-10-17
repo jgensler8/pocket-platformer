eb24def96c5e5f5fff0e31d7fdc723c2af890b46
Jeff Gensler <5685042+jgensler8@users.noreply.github.com>
2024-10-17 09:08:50 -0500

    get css bundling working and add write to disk to get serve writing to dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
 
 module.exports = {
@@ -6,12 +7,10 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: 'styles/[name].[ext]',  // Output location for SVGs
-                    },
-                },
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                ],
             },
             {
                 test: /\.(svg|png)$/, // Handle SVG files
@@ -36,11 +35,15 @@ module.exports = {
         },
         compress: true,
         port: 9000,
+        devMiddleware: {
+            writeToDisk: true,
+        },
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html', // Path to your existing index.html
             filename: 'index.html', // Output HTML file name
         }),
+        new MiniCssExtractPlugin(),
     ],
 };
